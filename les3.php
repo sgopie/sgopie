3f642f31849bf1615d520910f33d6dfeceb062f2
sgopie <302585768@student.rocmondriaan.nl>
2024-11-21 10:27:44 +0100

    update in les 3

--- les3.php
+++ les3.php
@@ -7,57 +7,66 @@
     <title>Hello world example</title>
 </head>
 <body>
-    <?php
-    echo "Hello World";
-    echo "<br>";
-    ?>
+<?php
+echo "Hello World";
+echo "<br>";
+?>
 
-    <?php
-    $text = 'Hello World Again'; //string
-    $age = 17; //integer
-    $price = 99.99; //float
-    $check = true; //boolean
-    echo "<br>";
-    echo "deze tekst lees je opnieuw: $text <br>";
-    ?>
+<?php
+$text = 'Hello World Again'; //string
+$age = 17; //integer
+$price = 99.99; //float
+$check = true; //boolean
+echo "<br>";
+echo "deze tekst lees je opnieuw: $text <br>";
+?>
 
-    <?php
+<?php
 //    normal array
-    echo "<br>";
-    $cars = array('Lamborghini','Bugatti','Mercedes','Toyota','Porsche');
-    print_r($cars);
-    echo "<br>";
-    echo "<br>";
-    print_r($cars[0]);
-    echo "<br>";
-    print_r($cars[1]);
-    echo "<br>";
-    print_r($cars[2]);
-    echo "<br>";
-    print_r($cars[3]);
-    echo "<br>";
-    print_r($cars[4]);
-    echo "<br>";
-    echo "Mijn fav car is: $cars[4]";
-    echo "<br>";
-    echo "<br>";
+echo "<br>";
+$cars = array('Lamborghini', 'Bugatti', 'Mercedes', 'Toyota', 'Porsche');
+print_r($cars);
+echo "<br>";
+echo "<br>";
+print_r($cars[0]);
+echo "<br>";
+print_r($cars[1]);
+echo "<br>";
+print_r($cars[2]);
+echo "<br>";
+print_r($cars[3]);
+echo "<br>";
+print_r($cars[4]);
+echo "<br>";
+echo "Mijn fav car is: $cars[4]";
+echo "<br>";
+echo "<br>";
 //    array with loop
-    $arrlength = count($cars);
-    for ($x = 0; $x < $arrlength; $x++){
-        echo $cars[$x];
-        echo "<br>";
-    }
-    ?>
-
-    <?php
-    $ages = array(
-            'Peter' => 35,
-            'Ben' => 37,
-            'Klaas' => 26,
-    );
-    print_r($ages);
+$arrlength = count($cars);
+for ($x = 0; $x < $arrlength; $x++) {
+    echo $cars[$x];
     echo "<br>";
-    echo 'de leeftijd van ben is ' .$ages['Ben']. ' jaar';
-    ?>
+}
+?>
+
+<?php
+$ages = array(
+    'Peter' => 35,
+    'Ben' => 37,
+    'Klaas' => 26,
+);
+print_r($ages);
+echo "<br>";
+echo 'de leeftijd van ben is ' . $ages['Ben'] . ' jaar';
+?>
+
+<?php
+try {
+    $db = new PDO("mysql:host=localhost;dbname=cars",
+        "root", "");
+} catch (PDOException $e) {
+    die("Error!: " . $e->getMessage());
+}
+?>
 </body>
 </html>
