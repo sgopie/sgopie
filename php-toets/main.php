14d280244cca6b1fb59f27f0afd9be7e084dfed3
sgopie <143504771+sgopie@users.noreply.github.com>
2025-01-31 02:51:37 +0100

    sneller gemaakt

--- php-toets/main.php
+++ php-toets/main.php
@@ -30,7 +30,7 @@ $vendors = $query->fetchAll(PDO::FETCH_ASSOC);
                 ?>
                 <div class="col-md-3">
                     <div class="card" style="width: 18rem;">
-                        <img src="<?php echo 'img/vendors/'.$vendor['image'];?>" class="card-img-top" alt="...">
+                        <img src="<?= 'img/vendors/'.$vendor['image']?>" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title"><?php echo $vendor['name'];?></h5>
                             <p class="card-text"><?php echo $vendor['description'];?></p>
